9c789ea172feed7f82e4058903470c175204cca5
Anthony Akentiev <anthony.akentiev@gmail.com>
2017-09-03 11:38:20 +0300

    Fixing ICO contract after review#2

--- contracts/EthLendICO.sol
+++ contracts/EthLendICO.sol
@@ -24,34 +24,34 @@ contract SafeMath {
 contract Token is SafeMath {
      // Functions:
      /// @return total amount of tokens
-     function totalSupply() constant returns (uint256 supply) {}
+     function totalSupply() constant returns (uint256 supply);
 
      /// @param _owner The address from which the balance will be retrieved
      /// @return The balance
-     function balanceOf(address _owner) constant returns (uint256 balance) {}
+     function balanceOf(address _owner) constant returns (uint256 balance);
 
      /// @notice send `_value` token to `_to` from `msg.sender`
      /// @param _to The address of the recipient
      /// @param _value The amount of token to be transferred
-     function transfer(address _to, uint256 _value) returns(bool){}
+     function transfer(address _to, uint256 _value) returns(bool);
 
      /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
      /// @param _from The address of the sender
      /// @param _to The address of the recipient
      /// @param _value The amount of token to be transferred
      /// @return Whether the transfer was successful or not
-     function transferFrom(address _from, address _to, uint256 _value) returns(bool){}
+     function transferFrom(address _from, address _to, uint256 _value) returns(bool);
 
      /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
      /// @param _spender The address of the account able to transfer the tokens
      /// @param _value The amount of wei to be approved for transfer
      /// @return Whether the approval was successful or not
-     function approve(address _spender, uint256 _value) returns (bool success) {}
+     function approve(address _spender, uint256 _value) returns (bool success);
 
      /// @param _owner The address of the account owning tokens
      /// @param _spender The address of the account able to transfer the tokens
      /// @return Amount of remaining tokens allowed to spent
-     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+     function allowance(address _owner, address _spender) constant returns (uint256 remaining);
 
      // Events:
      event Transfer(address indexed _from, address indexed _to, uint256 _value);
@@ -62,7 +62,7 @@ contract StdToken is Token {
      // Fields:
      mapping(address => uint256) balances;
      mapping (address => mapping (address => uint256)) allowed;
-     uint public totalSupply = 0;
+     uint public supply = 0;
 
      // Functions:
      function transfer(address _to, uint256 _value) returns(bool) {
@@ -89,18 +89,28 @@ contract StdToken is Token {
           return true;
      }
 
-     function balanceOf(address _owner) constant returns (uint256 balance) {
+     function totalSupply() constant returns (uint256) {
+          return supply;
+     }
+
+     function balanceOf(address _owner) constant returns (uint256) {
           return balances[_owner];
      }
 
-     function approve(address _spender, uint256 _value) returns (bool success) {
+     function approve(address _spender, uint256 _value) returns (bool) {
+          // To change the approve amount you first have to reduce the addresses`
+          //  allowance to zero by calling `approve(_spender, 0)` if it is not
+          //  already 0 to mitigate the race condition described here:
+          //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+          require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
           allowed[msg.sender][_spender] = _value;
           Approval(msg.sender, _spender, _value);
 
           return true;
      }
 
-     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+     function allowance(address _owner, address _spender) constant returns (uint256) {
           return allowed[_owner][_spender];
      }
 }
@@ -184,7 +194,7 @@ contract EthLendToken is StdToken
         // send team bonus immediately
         uint teamBonus = DEVELOPERS_BONUS;
         balances[_teamTokenBonus] += teamBonus;
-        totalSupply += teamBonus;
+        supply+= teamBonus;
 
         assert(PRESALE_TOKEN_SUPPLY_LIMIT==60000000 * (1 ether / 1 wei));
         assert(TOTAL_SOLD_TOKEN_SUPPLY_LIMIT==1000000000 * (1 ether / 1 wei));
@@ -210,7 +220,7 @@ contract EthLendToken is StdToken
         require(presaleSoldTokens + newTokens <= PRESALE_TOKEN_SUPPLY_LIMIT);
 
         balances[_buyer] += newTokens;
-        totalSupply += newTokens;
+        supply+= newTokens;
         presaleSoldTokens+= newTokens;
         totalSoldTokens+= newTokens;
 
@@ -226,7 +236,7 @@ contract EthLendToken is StdToken
         require(totalSoldTokens + newTokens <= TOTAL_SOLD_TOKEN_SUPPLY_LIMIT);
 
         balances[_buyer] += newTokens;
-        totalSupply += newTokens;
+        supply+= newTokens;
         icoSoldTokens+= newTokens;
         totalSoldTokens+= newTokens;
 
